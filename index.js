8b49715a0a4c2b2e988e2c020989e03e6331313a
ArnT <anders_soderberg@hotmail.com>
2020-05-10 15:34:20 +0200

    logging port and testing response

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const app = express()
+const port = process.env.port || 3000
 
-app.listen(3000)
+app.get('/', (req, res) => {
+    res.send('Server up and running')
+})
+
+app.listen(port, () => console.log('Server started on port', port))

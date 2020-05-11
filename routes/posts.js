cbb27077f9adaeaab10cf71182ce9bc87e384acd
ArnT <anders_soderberg@hotmail.com>
2020-05-11 12:28:11 +0200

    get all posts and create post done

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const router = express.Router()
+const Posts = require('../models/post')
 
 //GETTING ALL
-router.get('/', (req, res) => res.send('Hello from posts'))
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.find()
+        res.send(posts)
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
 //GETTING ONE
+router.get('/:_id', (req, res) => {
+    res.json(req.post)
+})
+
 //CREATING ONE
+router.post('/', async (req, res) => {
+    const post = new Posts({
+        title: req.body.title,
+        text: req.body.text
+    })
+    try {
+        const newPost = await post.save()
+        res.status(201).json(newPost)
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
 //UPDATING ONE
 //DELETE ONE
 

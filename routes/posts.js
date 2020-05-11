af62b13447dde159b6d1f7236665d48c4ea131e1
ArnT <anders_soderberg@hotmail.com>
2020-05-11 18:33:34 +0200

    methods for posts done

--- routes/posts.js
+++ routes/posts.js
@@ -14,8 +14,9 @@ router.get('/', async (req, res) => {
 })
 
 //GETTING ONE
-router.get('/:_id', (req, res) => {
+router.get('/:id', getPost, (req, res) => {
     res.json(req.post)
+    console.log(req)
 })
 
 //CREATING ONE
@@ -33,6 +34,49 @@ router.post('/', async (req, res) => {
     }
 })
 //UPDATING ONE
+router.patch('/:_id', getPost, async (req, res) => {
+    if (req.body.title) {
+        req.post.title = req.body.title
+        console.log('Changing title to', req.body.title)
+    }
+    if (req.body.text) {
+        req.post.text = req.body.text
+        console.log('Changing text to', req.body.text)
+    }
+    try {
+        const updatePost = await req.post.save()
+        res.json(updatePost)
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
+
 //DELETE ONE
+router.delete('/:_id', getPost, async (req, res) => {
+    try {
+        await req.post.remove()
+        res.json({ message: 'Deleted Post' })
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+//MIDDLEWARE FOR FINDING USER
+async function getPost(req, res, next) {
+    let post
+    try {
+        post = await Posts.findById(req.params._id)
+        if (post == null) {
+            return res.status(404).json({ message: 'Cannot find post' })
+        }
+    }
+    catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+    req.post = post
+    next()
+}
 
 module.exports = router
